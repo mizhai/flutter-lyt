835f47332bc40fa455f3d1b91c6264a494b05f07
luoyt <501244162@qq.com>
2019-03-01 13:58:56 +0800

    例子

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,10 +8,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "wlcome to Flutter",
-      theme: ThemeData(primaryColor: Colors.white),
-      home: RandomWords()
-    );
+        title: "wlcome to Flutter",
+        theme: ThemeData(primaryColor: Colors.white),
+        home: RandomWords());
   }
 }
 
@@ -21,19 +21,35 @@ class RandomWordsState extends State<RandomWords> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("name Generator")),
+//        appBar: AppBar(title: Text("name Generator")),
         body: _buildSuggestions());
   }
 
   Widget _buildSuggestions() {
-
+    /*
     return RaisedButton(
       onPressed: _launchUrl,
       child: Text("show Flutter homepage"),
-    );
+    );*/
 
-/*
-    return ListView.builder(
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("例子"),
+      ),
+      body: ListView.builder(
+          padding: const EdgeInsets.all(10.0),
+          itemBuilder: (context, i) {
+            if (i.isOdd) return Divider();
+            final index = i ~/ 2;
+            if (index >= _suggestions.length) {
+              _suggestions.addAll(generateWordPairs().take(10));
+            }
+            return _buildRow(_suggestions[index]);
+          }),
+      floatingActionButton: new FloatingActionButton(
+          tooltip: 'Add', child: new Icon(Icons.add), onPressed: null),
+    );
+    /*return ListView.builder(
         padding: const EdgeInsets.all(10.0),
         itemBuilder: (context, i) {
           if (i.isOdd) return Divider();
@@ -46,8 +62,8 @@ class RandomWordsState extends State<RandomWords> {
   }
 
   _launchUrl() async {
-    const url = "https://flutter.io";
-
+    const url = "https://m.pozhu.com";
+    await launch(url);
   }
 
   Widget _buildRow(WordPair pair) {
@@ -56,6 +72,19 @@ class RandomWordsState extends State<RandomWords> {
       pair.asPascalCase,
       style: _biggerFont,
     ));
+
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("例子"),
+      ),
+      body: ListTile(
+          title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      )),
+      floatingActionButton: new FloatingActionButton(
+          tooltip: 'Add', child: new Icon(Icons.add), onPressed: null),
+    );
   }
 }
 
